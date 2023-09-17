1070a78794053757502decfb0841b247abe395fa
Zach <zach.middleton12@yahoo.com>
2023-09-17 11:52:54 -0600

    changes for URI path and view section, reverted gitignore

---
+++ portfolio_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+
+    # Render the HTML template index.html with the data in the context variable
+    return render( request, 'portfolio_app/index.html')
