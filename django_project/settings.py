ec2e999fb38ee0a6529802e793051ccbe674ad3c
Zach <zach.middleton12@yahoo.com>
2023-09-17 12:09:35 -0600

    added logo image

--- django_project/settings.py
+++ django_project/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -122,3 +123,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+
+STATIC_URL = 'static/'
+
+
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, 'static')
+]
+
+
+MEDIA_URL = '/images/'
